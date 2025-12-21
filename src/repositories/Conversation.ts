b79a199a5511da027529e8b9b49744eddb5ec04d
Ryan <ryansoaresjorvino@gmail.com>
2025-12-21 16:49:59 -0300

    feature: Adicionado repository para Item

--- src/repositories/Conversation.ts
+++ src/repositories/Conversation.ts
@@ -4,15 +4,12 @@ import { Repository } from "typeorm";
 
 export default class ConversationRepository implements ConversationInterfaceRepository {
     private repository: Repository<Conversation>;
-
     constructor(repository: Repository<Conversation>) {
         this.repository = repository;
     }
-
     async save(conversation: Conversation): Promise<Conversation> {
         return await this.repository.save(conversation);
     }
-
     async getMessages(): Promise<void> {
         await this.repository.find();
     }
---
+++ src/repositories/Item.ts
@@ -0,0 +1,76 @@
+import ItemInterfaceRepository from "./interfaces/Item.ts";
+import {Repository} from "typeorm";
+import Item from "../entities/Item.ts";
+
+export default class ItemRepository implements ItemInterfaceRepository {
+    private repository: Repository<Item>;
+    constructor(repository: Repository<Item>) {
+        this.repository = repository;
+    }
+    async create(item: Item): Promise<{success: boolean; message?: string}> {
+        try {
+            await this.repository.save(item)
+            return {
+                success: true,
+                message: "Item criado com sucesso!"
+            }
+        } catch (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: "Erro ao criar um Item!"
+            }
+        }
+    }
+    async list(): Promise<void> {
+        await this.repository.find()
+    }
+    async update(id: number, item: Item): Promise<{ success: boolean; message: string }> {
+        try {
+            const itemToUpdate = await this.repository.findOneBy({id: id});
+
+            if (!itemToUpdate) {
+                return {
+                    "success": false,
+                    "message": "Nenhum Item encontrado com esse id!"
+                }
+            }
+
+            itemToUpdate.id = id;
+            itemToUpdate.name = item.name;
+            itemToUpdate.description = item.description;
+            itemToUpdate.amount = item.amount;
+            itemToUpdate.price = item.price;
+
+            await this.repository.save(itemToUpdate);
+
+            return {
+                success: true,
+                message: "Item atualizado com sucesso!"
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                success: false,
+                message: "Erro ao atualizar o Item!"
+            }
+        }
+    }
+    async delete(id: number): Promise<{ success: boolean; message: string }> {
+        try {
+            await this.repository.delete(id)
+
+            return {
+                success: true,
+                message: "Item deletado com sucesso!"
+            }
+
+        } catch (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: "Erro ao deletar um Item!"
+            }
+        }
+    }
+}
