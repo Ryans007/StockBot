577e5a7ef805410c8eae8584167bec4e88a3478b
Ryan <ryansoaresjorvino@gmail.com>
2025-12-24 12:24:27 -0300

    refactor: Utilização do zod para validar os dados do request body da rota de assistant

--- src/controllers/Conversation.ts
+++ src/controllers/Conversation.ts
@@ -3,39 +3,26 @@ import { Request, Response } from "express";
 import * as crypto from "crypto";
 import graph from "../graph/graph";
 import Conversation from "../entities/Conversation";
-import {ConversationSchema} from "../schemas/Conversation.ts";
-
-interface ChatRequest {
-    user_message: string;
-    thread_id?: string;
-}
+import { ConversationSchema } from "../schemas/Conversation.ts";
 
 export default class ConversationController {
     constructor(private repository: ConversationRepository) { };
     async createConversation(req: Request, res: Response) {
-        const { user_message, thread_id } = <ChatRequest>req.body;
-
-        const activeThreadID = thread_id || crypto.randomUUID();
-
+        const validatedData = ConversationSchema.parse(req.body);
+        const activeThreadID = validatedData.thread_id || crypto.randomUUID();
         const config = { "configurable": { "thread_id": activeThreadID } };
-        const response = await graph.invoke({ "userInput": user_message }, config);
-        console.log(response);
-
+        const response = await graph.invoke({ "userInput": validatedData.user_message }, config);
         const ai_message = response.finalAnswer;
-
         const conversation = new Conversation(
             activeThreadID,
-            user_message,
+            validatedData.user_message,
             String(ai_message)
         )
-
         await this.repository.save(conversation);
-
         return res.status(201).json(conversation);
     }
     async getMessages(_: Request, res: Response) {
         const conversationList = await this.repository.getMessages();
-
         return res.status(201).json(conversationList);
     }
 }
