4a09173446287df83993255ab356865e25a4e3ab
Ryan <ryansoaresjorvino@gmail.com>
2025-12-25 16:30:38 -0300

    feature: Adição de containerização ao sistema

--- backend/src/config/dataSource.ts
+++ backend/src/config/dataSource.ts
@@ -1,10 +1,18 @@
 import {DataSource} from "typeorm";
 import path from "path";
+import Item from "../entities/Item.ts";
+import Recipe from "../entities/Recipe.ts";
+import Conversation from "../entities/Conversation.ts";
+import dotenv from "dotenv";
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "./src/config/database.sqlite",
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT || "5432"),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: true,
     logging: false,
-    entities: ["src/entities/**/*.ts"],
+    entities: [Item, Recipe, Conversation],
 });
